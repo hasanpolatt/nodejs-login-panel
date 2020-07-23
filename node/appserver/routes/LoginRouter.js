895e0a824578485cff8bffd134153fa6333f4fdb
Hasan Polat <thatsnotpublic@gmail.com>
2020-07-23 17:41:30 +0300

    Update LoginRouter.js

--- node/appserver/routes/LoginRouter.js
+++ node/appserver/routes/LoginRouter.js
@@ -6,5 +6,9 @@ var ctrlLogin = require('../controller/LoginCont');
 
 router.get('/', ctrlLogin.indexGEt);
 router.post('/', ctrlLogin.indexPost);
+router.get('/signup', ctrlLogin.signupGet);
+router.post('/signup',ctrlLogin.signupPost);
+router.get('/userlist', ctrlLogin.userlist);
+router.get('/userdelete/:username', ctrlLogin.userDelete);
 
 module.exports = router;
